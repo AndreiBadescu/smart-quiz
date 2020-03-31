213233ae0d46ffe3bc84204882d6ec70078ade85
AndreiBadescu <34285711+AndreiBadescu@users.noreply.github.com>
2020-03-31 18:03:27 +0300

    Update Q&A.js

--- js/Q&A.js
+++ js/Q&A.js
@@ -20,7 +20,7 @@ questions = [
 	key: 3
 	},
 	{
-	question: "Care este numele virusului care a produs pandemia din 2020 ?",
+	question: "Care este numele virusului care a produs pandemia din 2019 ?",
 	answers: [
 	    "Covid-19",
 	    "Sars-Cov-2",
